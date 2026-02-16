bde32b7658ade3e468360943bc573556133a3193
EbraamSobhy <ibraam.sobhy2003@gmail.com>
2026-02-16 13:29:31 +0200

    changes

--- app/manifest.js
+++ app/manifest.js
@@ -2,22 +2,20 @@ export default function manifest() {
   return {
     name: "DevPath",
     short_name: "DevPath",
+    id: "/",
     description: "Roadmap Web App for CS Students and Beginners",
     start_url: "/",
+    scope: "/",
     display: "standalone",
     background_color: "#000075",
     theme_color: "#000075",
     orientation: "portrait",
     icons: [
       {
-        src: "/DevPathLogo.png",
+        src: "/DevPath icon.png",
         sizes: "1024x1024",
         type: "image/png",
-      },
-      {
-        src: "/DevPath icon.png",
-        sizes: "263x329",
-        type: "image/png",
+        purpose: "any",
       },
     ],
   };
